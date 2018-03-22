3a797c4cef0432ef002955a0f07a4332d1e1ce0a
Vinoth <vinothgramesh@gmail.com>
2018-03-22 20:52:24 +0530

    'tokenCreation'

--- mongoose/mongoose.js
+++ mongoose/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
 mongoose.promise=global.promise;
-mongoose.connect('mongodb://localhost:27017/TodoApps');
+mongoose.connect('mongodb://localhost:27017/User');
 
 module.exports={mongoose};
--- mongoose/user_server.js
+++ mongoose/user_server.js
@@ -3,7 +3,7 @@ const bodyParser=require('body-Parser');
 const {ObjectId}=require('mongodb');
 const _=require('lodash');
 
-var {Todo}=require('./Todo');
+var {User}=require('./user');
 var {mongoose}=require('./mongoose')
 
 var app=express();
@@ -11,19 +11,18 @@ const port=process.env.port || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-  var text=JSON.stringify(req.body.text);
-  var todo=new Todo({
-    text: text,
-    completed:req.body.completed,
-    completedAt:req.body.completedAt
-  });
-  todo.save().then((doc)=>{
-  res.send(doc);
-  },(e)=>{
-  res.send(e);
-  });
-  });
+app.post('/user',(req,res)=>{
+  var body=_.pick(req.body,['email','password']);
+  var user=new User(body);
+
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  }).then((token)=>{
+    res.header('x-auth',token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  })
+});
   app.get('/todos',(req,res)=>{
     Todo.find().then((todos)=>{
       res.send({todos});
